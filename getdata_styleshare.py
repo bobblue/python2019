893afac6f7d9ed5975f7db0f022663752edb5f56
seoulblanc <seoulblanc@gmail.com>
2019-02-08 15:07:27 +0900

    'styleshare'

---
+++ getdata_styleshare.py
@@ -0,0 +1,146 @@
+import requests
+import time
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+from bs4 import BeautifulSoup
+import re
+import csv
+from itertools import count
+
+
+def get_information(url):
+    try:
+        req = requests.get(url)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        # print("URL request Success")
+    except Exception as e:
+        print("Error for URL")
+
+    productname = []
+    username = []
+    content = []
+    like_count = []
+    comment_count = []
+
+    try:
+        products = soup.select('a.goods-name')
+        productname.append(products[0].text)
+    except Exception as e:
+        productname.append('정보없음')
+        print("product 정보없음")
+
+    try:
+        usernames = soup.select('a.nickname')
+        for i in usernames:
+            user = i.text
+            username.append(user)
+    except Exception as e:
+        username.append('정보없음')
+        print("username 정보없음")
+
+    try:
+        contents = soup.select('div.description-wrapper > p.description')
+        for i in contents:
+            con = i.text
+            con = con.replace('\n', '')
+            con = con.replace(' ', '')
+            content.append(con)
+    except Exception as e:
+        content.append('정보없음')
+        print("content 정보없음")
+
+    try:
+        likes = soup.select('button.list-of-likes')
+        for i in likes:
+            like_ = i.text
+            like_count.append(like_)
+    except Exception as e:
+        like_count.append('정보없음')
+        print("like_count 정보없음")
+
+    try:
+        comments = soup.select('p.comments-count > span.op-count')
+        for i in comments:
+            comment_ = i.text
+            comment_ = comment_.replace('(', '')
+            comment_ = comment_.replace(')', '')
+            comment_count.append(comment_)
+    except Exception as e:
+        comment_count.append('정보없음')
+        print("comment_count 정보없음")
+
+    df2 = pd.DataFrame({'product_name': productname, 'username': username, 'content': content, 'like_count': like_count,
+                        'comment_count': comment_count, 'url': url})
+    return df2
+
+
+def make_url(page, keyword):
+    url_01 = 'https://www.stylesha.re/search/styles?limit=20&keyword='
+    url_02 = '&offset='
+    url = url_01 + keyword + url_02 + page
+    print(url)
+    return url
+
+
+def get_data(url):
+    try:
+        req = requests.get(url)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        print("URL request Success")
+    except Exception as e:
+        print("Error for URL")
+
+    url_each = soup.select('a.move-to-fullview')
+
+    result = []
+    for i in url_each:
+        each_shop = i.get('href')
+        # print(each_shop)
+        url_front = 'https://www.stylesha.re'
+        reviewpage_url = url_front + each_shop
+        result.append(reviewpage_url)
+
+    df1 = pd.DataFrame({'url_list': result})
+    print(len(df1))
+    return df1
+
+
+def get_url_dataframe():
+    data_result = pd.DataFrame()
+    for i in range(0, 20000):
+        keyword = '운동화'
+        page = str(i * 20)
+        url = make_url(page, keyword)
+        df1 = get_data(url)
+        if len(df1) == 0:
+            break
+        else:
+            pass
+        data_result = pd.concat([data_result, df1], axis=0)
+    data_result.to_excel('C:/Users/leevi/Downloads/styleshare_{}_URL.xlsx'.format(keyword), index=False)
+    print('ULR 정보 저장 완료')
+    data = data_result
+    return data, keyword
+
+
+def main():
+    data, keyword = get_url_dataframe()
+    data_result = pd.DataFrame()
+    for url in data['url_list']:
+        try:
+            df2 = get_information(url)
+        except Exception as e:
+            print("Error for URL")
+
+        data_result = pd.concat([data_result, df2], axis=0)
+    data_result.to_excel('C:/Users/leevi/Downloads/styleshare_{}_RESULT.xlsx'.format(keyword), index=False)
+    print('RESULT 저장 완료')
+
+
+if __name__ == "__main__":
+    main()
+
+
