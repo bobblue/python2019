82e4ced381d124ceb9fb7dc96713f6e2c2e9a284
YOONAH KIM <37688086+seoulblanc@users.noreply.github.com>
2019-03-10 15:36:20 +0900

    Update pc_mobile_cnt.py

--- pc_mobile_cnt.py
+++ pc_mobile_cnt.py
@@ -1,3 +1,5 @@
+# 네이버 광고 사이트 '키워드' 검색량 분석 
+
 import pandas as pd
 
 json = {"keywordList":[{"userStat":{"monthlyPcQcCnt":[20,30,750,1260,720,1260,940,1700,1080,2510,1150,1930,290,670],"monthlyMobileQcCnt":[2300,1580,45100,37700,16100,12500,11900,11300,26200,23100,35800,23100,6890,6320],"genderType":["f","m","f","m","f","m","f","m","f","m","f","m","f","m"],"ageGroup":["0-12","0-12","13-19","13-19","20-24","20-24","25-29","25-29","30-39","30-39","40-49","40-49","50-","50-"]},"relKeyword":"데상트","monthlyProgressList":{"monthlyProgressPcQcCnt":[83600,83900,77300,74100,63300,67800,65100,82300,111200,111100,81300,66400,58900],"monthlyProgressMobileQcCnt":[310400,242200,228000,225700,209100,233200,243300,336500,444500,417000,351200,286300,314700],"monthlyLabel":["2018-02","2018-03","2018-04","2018-05","2018-06","2018-07","2018-08","2018-09","2018-10","2018-11","2018-12","2019-01","2019-02"]}}]}
@@ -44,7 +46,7 @@ m_json.update({'40대이상' : {'m_pc' :sum(m.loc[10:, :]['month_pc']), "m_mobil
 print('남성 검색량(최근 한 달)')
 print(pd.DataFrame(m_json))
 
-#with pd.ExcelWriter('naverkeyword_cnt.xlsx') as writer:
-#    year_df.to_excel(writer, sheet_name='Sheet1')  # Default position, cell A1.
-#    pd.DataFrame(f_json).to_excel(writer, sheet_name='Sheet1', startcol=5)
-#    pd.DataFrame(m_json).to_excel(writer, sheet_name='Sheet1', startcol=5, startrow=5)
+with pd.ExcelWriter('naverkeyword_cnt.xlsx') as writer:
+    year_df.to_excel(writer, sheet_name='Sheet1')  # Default position, cell A1.
+    pd.DataFrame(f_json).to_excel(writer, sheet_name='Sheet1', startcol=5)
+    pd.DataFrame(m_json).to_excel(writer, sheet_name='Sheet1', startcol=5, startrow=5)
